7c80b3dad887c058a239ea2036584c04a2a28704
Otavio Faria <oaugustofaria@gmail.com>
2021-11-16 21:45:03 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text;
 using xadrez_console.tabuleiro;
+using xadrez_console.xadrez;
 
 namespace xadrez_console
 {
@@ -7,10 +9,19 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            //RichTextBox console = new RichTextBox();
+           // console.Font = new Font("Consolas", 12);
+
             Tabuleiro tab = new Tabuleiro(8,8);
 
-            Tela.imprimirTabuleiro(tab);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
 
+
+            Tela.imprimirTabuleiro(tab);
+            
             Console.WriteLine();
 
 
---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using xadrez_console.tabuleiro;
+
+namespace xadrez_console.xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "\u265A";
+        }
+    }
+}
